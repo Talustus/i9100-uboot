629b578020455d40cabb883ef342c9d2547bc76c
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-03-10 20:10:41 +0400

    Add GALAXYS2 mtype

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -1105,6 +1105,7 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_UBISYS_P9D_EVP       3493
 #define MACH_TYPE_ATDGP318             3494
 #define MACH_TYPE_OMAP5_SEVM           3777
+#define MACH_TYPE_GALAXYS2            3963
 
 #ifdef CONFIG_ARCH_EBSA110
 # ifdef machine_arch_type
@@ -14220,6 +14221,18 @@ extern unsigned int __machine_arch_type;
 # define machine_is_omap5_sevm()      (machine_arch_type == MACH_TYPE_OMAP5_SEVM)
 #else
 # define machine_is_omap5_sevm()      (0)
+#endif
+
+#ifdef CONFIG_MACH_GALAXYS2
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type     __machine_arch_type
+# else
+#  define machine_arch_type     MACH_TYPE_GALAXYS2
+# endif
+# define machine_is_galaxys2()      (machine_arch_type == MACH_TYPE_GALAXYS2)
+#else
+# define machine_is_galaxys2()      (0)
 #endif
 
 /*
